aca9fc25eee76c8571e86ddc3682b82cc3997f4c
xnpst <xnpster@yandex.ru>
2021-09-08 17:31:38 +0300

    minimal testfiles

---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by xnpst on 08.09.2021.
+//
+
+#ifndef C_TESTER_MAIN_H
+#define C_TESTER_MAIN_H
+
+#endif //C_TESTER_MAIN_H
---
+++ testing_program/parse-testfile.c
@@ -0,0 +1,4 @@
+//
+// Created by xnpst on 08.09.2021.
+//
+
